500a5834ae237a1536bcd862fd2f576a02ccdb9d
Alexandre Bulté <alexandre@bulte.net>
2019-10-04 14:13:48 +0200

    init guide qualité

---
+++ .vuepress/config.js
@@ -0,0 +1,21 @@
+sidebar = [
+  {
+    title: 'Guide qualité',
+    path: '/qualite',
+    collapsable: false,
+    children: [
+      'qualite/1-preparer-le-jeu-de-donnees',
+      'qualite/2-documenter-les-donnees',
+      'qualite/3-elements-organisationnels'
+    ]
+  },
+]
+
+module.exports = {
+  title: 'guides.etalab.gouv.fr',
+  description: 'Les guides d\'Etalab',
+  themeConfig: {
+    sidebar: sidebar,
+    sidebarDepth: 1,
+  }
+}
