51cb7191c683f48b531913c31b0a239dff1f334b
Antoine Augusti <antoine.augusti@data.gouv.fr>
2019-12-18 16:10:39 +0100

    Accueil : sidebar custom par groupes

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,11 +1,45 @@
 const {getSidebar} = require('vuepress-theme-gouv-fr/sidebar.js')
 
+function sidebar () {
+  data = getSidebar()
+
+  data['/'] = [
+    {
+      title: 'Ouverture et circulation des données',
+      collapsable: false,
+      children: [
+        ['/qualite/', "Comment préparer des données à l'ouverture / la circulation ?"],
+        ['/juridique/', "Quels jeux de données doivent être publiés en open data ?"],
+        ['/data.gouv.fr/', "Comment publier des jeux de données sur data.gouv.fr ?"],
+      ]
+    },
+    {
+      title: 'Algorithmes publics',
+      collapsable: false,
+      children: [
+        ['/algorithmes/', "Les algorithmes publics : pourquoi et comment les expliquer ?"],
+      ]
+    },
+    {
+      title: 'Codes sources',
+      collapsable: false,
+      children: [
+        ['/logiciels/', "Codes sources du secteur public : lesquels ouvrir, pourquoi et comment ?"],
+      ]
+    },
+  ]
+
+  return data
+}
+
+sidebar();
+
 module.exports = {
   title: 'guides.etalab.gouv.fr',
   description: "Les guides d'Etalab : vous accompagner dans la réalisation de vos projets relatifs aux données, algorithmes et codes sources.",
   theme: 'vuepress-theme-gouv-fr',
   themeConfig: {
-    sidebar: getSidebar(),
+    sidebar: sidebar(),
     sidebarDepth: 1,
     logo: '/images/logo-marianne.svg',
     // lastUpdated: 'Dernière mise à jour',
