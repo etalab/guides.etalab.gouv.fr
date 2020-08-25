6f597860672d9036f04278d5d27a28ca4e28d5dc
Antoine Augusti <antoine.augusti@data.gouv.fr>
2020-08-25 10:20:03 -0400

    Switch to GitHub Actions and GH pages & build PDF exports (#81)
    * Switch to GitHub Actions and GH pages
    
    * Cache VuePress files
    
    * Upload artifact
    
    * Try to use LaTeX container
    
    * Multiple jobs
    
    * Indentation
    
    * Remove source
    
    * Source command
    
    * Set paths
    
    * Add page for PDF exports
    
    * Typo
    
    * Target blank
    
    * Add back now.json
    
    * Change org for pandoc-guides
    
    * Remove unnecessary check

---
+++ .vuepress/components/PDFExports.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="item in pdfFiles" :key="item.path">
+        <a :href="item.path" target="_blank">📂 {{ item.name }}</a>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  name: 'PDFExports',
+  computed: {
+    pdfFiles () {
+      return this.$site.themeConfig.pdfFiles || []
+    }
+  }
+}
+</script>
